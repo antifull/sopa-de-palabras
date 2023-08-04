171f222a444e340cae3b0e6a3f09ad139c2c5a48
Francisco Rubio Santiesteban <115087513+antifull@users.noreply.github.com>
2023-08-03 22:58:22 -0400

    Agregando elementos en el control de excepciones.

--- src/main/java/com/ejemplo/sopadeletras/controller/SopaControllerAdvice.java
+++ src/main/java/com/ejemplo/sopadeletras/controller/SopaControllerAdvice.java
@@ -1,8 +1,25 @@
 package com.ejemplo.sopadeletras.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.util.NoSuchElementException;
 
-@ControllerAdvice
+@ControllerAdvice(basePackageClasses = SopaController.class)
 public class SopaControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException noSuchElementException){
+        return new ResponseEntity<>("No se encuentra el elemento en BD", HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException illegalArgumentException){
+        return new ResponseEntity<>("Elemento no valido",HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerException(NullPointerException nullPointerException){
+        return new ResponseEntity<>("Elemento no valido",HttpStatus.NOT_FOUND);
+    }
 }
